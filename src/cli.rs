3e3eea1eff239df0933e18633f53b714ab2a0ca7
mawngo <sitdownrightnow.dev@gmail.com>
2025-12-10 00:18:40 +0700

    feat(wip): add basic image parsing logic

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use image::ImageReader;
+use std::error::Error;
+use std::path::Path;
+use tracing::{error, info, warn};
 
 #[derive(Parser)]
 #[command(name = "kcomprs", about = "Reduce number of colors used in image")]
@@ -76,12 +80,108 @@ pub struct Cli {
     jpeg: u32,
 
     #[arg(long, action, help = "Enable debug mode")]
-    debug: bool,
+    pub debug: bool,
+}
+
+struct DecodedImage {
+    img: image::DynamicImage,
+    format: image::ImageFormat,
+    // TODO: use &str?
+    path: String,
 }
 
 impl Cli {
-    pub fn execute() -> Self {
-        let cli = Self::parse();
-        return cli;
+    pub fn new() -> Self {
+        let mut cli = Self::parse();
+        if cli.quick {
+            cli.delta = 0.01;
+            cli.round = 50
+        }
+        cli
     }
+
+    pub fn execute(self: Self) -> Result<usize, Box<dyn Error>> {
+        let mut count = 0;
+        let images = scan_images(&self.files);
+        for image in images {
+            match self.handle_image(&image) {
+                Ok(_) => count += 1,
+                Err(err) => {
+                    error!(error = err, path = image.path, "Error processing image");
+                }
+            }
+        }
+        Ok(count)
+    }
+
+    fn handle_image(&self, image: &DecodedImage) -> Result<(), Box<dyn Error>> {
+        let path = Path::new(&image.path);
+        let filename = path
+            .file_name()
+            .expect("Missing filename what the fuck")
+            .to_str();
+        if filename.is_none() {
+            return Err(format!("Invalid filename: {}", path.display()).into());
+        }
+
+        let format = image.format.extensions_str().join("|");
+        info!(
+            cp = self.colors,
+            round = self.round,
+            img = filename.unwrap(),
+            dimension = format!("{}x{}", image.img.width(), image.img.height()),
+            format = format,
+            "Processing image"
+        );
+        Ok(())
+    }
+}
+
+fn scan_images(files: &[String]) -> Vec<DecodedImage> {
+    let mut images: Vec<DecodedImage> = Vec::with_capacity(files.len());
+    for path in files {
+        let reader = ImageReader::open(&path);
+        if reader.is_err() {
+            error!(
+                path = &path,
+                error = reader.err().unwrap().get_ref(),
+                "Error opening image"
+            );
+            continue;
+        }
+        let reader = reader.unwrap().with_guessed_format();
+        if reader.is_err() {
+            error!(
+                path = &path,
+                error = reader.err().unwrap().get_ref(),
+                "Error reading image"
+            );
+            continue;
+        }
+        let reader = reader.unwrap();
+
+        let format = reader.format();
+        if format.is_none() {
+            warn!(path = &path, "Unsupported image format");
+            continue;
+        }
+
+        let img = reader.decode();
+        if img.is_err() {
+            let error = img.err().unwrap();
+            error!(
+                path = &path,
+                error = &error as &dyn Error,
+                "Error decoding image"
+            );
+            continue;
+        }
+
+        images.push(DecodedImage {
+            img: img.unwrap(),
+            format: format.unwrap(),
+            path: path.to_string(),
+        })
+    }
+    images
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 use crate::cli::Cli;
+use std::time::Instant;
+use tracing::{error, info};
 
 mod cli;
 
 fn main() {
-    Cli::execute();
+    let start = Instant::now();
+    let cli = Cli::new();
+
+    // Setup logger.
+    let logger = tracing_subscriber::fmt()
+        .with_file(false)
+        .with_line_number(false)
+        .with_target(false);
+    let logger = if cli.debug {
+        logger.with_max_level(tracing::Level::DEBUG)
+    } else {
+        logger.with_max_level(tracing::Level::INFO)
+    };
+    logger.init();
+
+    let res = cli.execute();
+    match res {
+        Ok(_) => {
+            info!(
+                ms = start.elapsed().as_millis(),
+                total = res.unwrap(),
+                "Processing completed"
+            );
+        }
+        Err(err) => {
+            error!(error = err, "Error occurred");
+        }
+    }
 }
